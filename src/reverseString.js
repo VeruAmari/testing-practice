4b5bd50099b88cad6bbc7567b985510726c1c2aa
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-04 15:18:31 -0300

    Write reverseString

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,3 +1,11 @@
 export default reverseString = (str) => {
-  return str;
+  if (!str) {
+    return str;
+  }
+  const safe = String(str);
+  let result = "";
+  for (let i = safe.length - 1; i >= 0; i--) {
+    result += safe[i];
+  }
+  return result;
 };
