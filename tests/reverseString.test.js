0b7df731fbf599f4541b34a6c399e0eafc5eaf3b
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-04 15:09:42 -0300

    Write tests for reverseString

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -1,5 +1,21 @@
 import reverseString from "../src/reverseString";
 
-test("empty", () => {
-  expect("a").toBe("a");
+test("it works on a regular string", () => {
+  expect(reverseString("abcdefg")).toBe("gfedcba");
+});
+
+test("it works on numbers", () => {
+  expect(reverseString(123456)).toBe("654321");
+});
+
+test("it works on mixed strings", () => {
+  expect(reverseString("abCD3f6")).toBe("6f3DCba");
+});
+
+test("it doesn't break on single characters", () => {
+  expect(reverseString("a")).toBe("a");
+});
+
+test("it doesn't break on empty strings", () => {
+  expect(reverseString("")).toBe("");
 });
