ca2407b513d7f487fca3b8543ac739edec836909
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-04 18:31:30 -0300

    Write tests for caesarCipher

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,5 +1,25 @@
 import caesarCipher from "../src/caesarCipher";
 
-test("empty", () => {
-  expect("a").toBe("a");
+test("basic cipher test", () => {
+  expect(caesarCipher("abc", 1)).toBe("bcd");
+});
+
+test("ciphers with negative value", () => {
+  expect(caesarCipher("abc", -1)).toBe("zab");
+});
+
+test("ciphers keys bigger than 24", () => {
+  expect(caesarCipher("abc", 30)).toBe("bcd");
+});
+
+test("returns plaintext when key is 0", () => {
+  expect(caesarCipher("abc", 0)).toBe("abc");
+});
+
+test("handles non numeric keys", () => {
+  () => expect(caesarCipher("abc", "a")).toThrow(Error);
+});
+
+test("applies a random cipher when no key", () => {
+  expect(caesarCipher("abc")).not.toBe("abc");
 });
