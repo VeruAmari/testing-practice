fc640b81d5d4d136250c752a253034416bc21dc5
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-05 10:20:21 -0300

    Write tests for analyzeArray

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,5 +1,46 @@
 import analyzeArray from "../src/analyzeArray";
 
-test("empty", () => {
-  expect("a").toBe("a");
+test("basic test", () => {
+  expect(analyzeArray([1, 3, 2])).toEqual({
+    average: 2,
+    min: 1,
+    max: 3,
+    length: 3,
+  });
+});
+
+test("handles empty array input", () => {
+  expect(analyzeArray([])).toEqual({
+    average: undefined,
+    min: undefined,
+    max: undefined,
+    length: 0,
+  });
+});
+
+test("handles arrays with a single item", () => {
+  expect(analyzeArray([1])).toEqual({
+    average: 1,
+    min: 1,
+    max: 1,
+    length: 1,
+  });
+});
+
+test("handles arrays with mixed characters", () => {
+  expect(analyzeArray(["a", ".", 3, 50, "@"])).toEqual({
+    average: 3,
+    min: ".",
+    max: "@",
+    length: 5,
+  });
+});
+
+test("handles arrays with only letters", () => {
+  expect(analyzeArray(["a", "d", "c"])).toEqual({
+    average: "c",
+    min: "a",
+    max: "d",
+    length: 3,
+  });
 });
