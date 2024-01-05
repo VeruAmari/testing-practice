2774c406c0ebc56b645b9563cb8f7260c732bfcd
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-05 10:21:24 -0300

    Write analyzeArray along with tests

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,6 +1,11 @@
 export default analyzeArray = (arr) => {
-  for (let item of arr) {
-    console.log(item);
-  }
-  return arr;
+  arr.sort((a, b) => {
+    return a < b ? -1 : 1;
+  });
+  const obj = {};
+  obj.length = arr.length || Number(0);
+  obj.average = arr[Math.floor(arr.length / 2)];
+  obj.min = arr[0];
+  obj.max = arr[arr.length - 1];
+  return obj;
 };
