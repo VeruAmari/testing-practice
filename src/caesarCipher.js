6bb21c1948caaea6df938ad0789401c5904b84a9
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-01-05 09:26:31 -0300

    Implement caesarCipher according to written test suite parameters

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,4 +1,13 @@
 export default caesarCipher = (plaintext, key) => {
+  if (!key && key !== 0) {
+    key = 0;
+    while (key === 0) {
+      key = Math.round(Math.random() * 100);
+    }
+  }
+  if (key < 0) {
+    key = 26 + key;
+  }
   if (key > 26) {
     key = Math.round(key % 26);
   }
@@ -30,20 +39,53 @@ export default caesarCipher = (plaintext, key) => {
     "y",
     "z",
   ];
+  const LETTERS = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
   let ciphered = "";
-  for (let letter of plaintext) {
-    if (letters.includes(letter.toLowerCase())) {
-      let safeKey = letters.indexOf(letter.toLowerCase()) + key;
-      if (safeKey > 26) {
-        safeKey = safeKey % 26;
-      }
-      console.log(safeKey);
-      const theLetter = letters[safeKey];
-      ciphered +=
-        letter.toLowerCase() === letter ? theLetter : theLetter.toUpperCase();
+
+  function cipher(arr, char) {
+    let safeKey = arr.indexOf(char) + key;
+    if (safeKey >= 26) {
+      safeKey = safeKey % 26;
+    }
+    console.log(safeKey);
+    const newChar = arr[safeKey];
+    ciphered += newChar;
+  }
+
+  for (let char of plaintext) {
+    if (letters.includes(char)) {
+      cipher(letters, char);
+    } else if (LETTERS.includes(char)) {
+      cipher(LETTERS, char);
     } else {
-      ciphered += letter;
+      ciphered += char;
     }
   }
   return ciphered;
